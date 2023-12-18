c8ede5892ca7bb40d914f9f6cbdabd7227ad1ddd
Jayasri c <inv097@innvectra.local>
2023-12-18 16:59:42 +0530

    first commit

---
+++ client/src/components/home/Room.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Room = () => {
+  return (
+    
+  
+    
+    <div className="card-stacked">
+      <div className="card-content">
+        <p>I am a very simple card. I am good at containing small bits of information.</p>
+      </div>
+    
+    </div>
+ 
+  )
+}
+
+export default Room
